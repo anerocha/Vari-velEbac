0e07097a97d8cb557cf216cdf5f12f22b7f0076c
Ane <anerocha1999@gmail.com>
2024-06-16 15:50:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function armazenarValor() {
+
+
+    var userInput = document.getElementById("userInput").value;
+    
+    console.log("O valor da variavel é: " + userInput);
+    
+    document.getElementById("valorInserido").innerText = "O valor inserido é: " + userInput;
+    
+    }
+    
